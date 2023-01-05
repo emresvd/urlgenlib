d929b2257fd2b5682565c8d7e4b7be76f0b32083
emresvd <emresevindi99@gmail.com>
2023-01-05 10:37:47 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-import urlgenlib
+import sys
 
-if __name__ == "__main__":
+if sys.argv[-1] == "main.py":
+    import urlgenlib
     print(
         urlgenlib.urlgen("https://github.com/emresvd/")
     )
+
+if sys.argv[-1] == "clean":
+    import shutil
+    for i in ["build", "dist", "urlgenlib.egg-info", "files"]:
+        shutil.rmtree(i)
+
+if sys.argv[-1] == "build":
+    import os
+    os.system("python setup.py sdist bdist_wheel")
+
+if sys.argv[-1] == "upload":
+    import os
+    os.system("twine upload dist/*")
